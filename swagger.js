e1f4035dd2dc3b10a8f5ab492683adc3b2c83e23
Austin Nelson <nel.austintyler.son@gmail.com>
2024-02-06 22:54:32 -0700

    lesson4

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Description'
   },
-  host: 'localhost:8080'
+  host: 'https://nelson-austin.onrender.com/'
 };
 
 const outputFile = './swagger-output.json';
